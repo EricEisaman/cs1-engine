3d6642537466a04798d635cd90e073c527100c63
Glitch (988) <none>
2020-05-23 20:43:06 +0000

    added cs1-trail lib

--- src/engine/modules/libmap.js
+++ src/engine/modules/libmap.js
@@ -2,7 +2,9 @@ export const libmap = {
   
 "cs1-jukebox": "https://raw.githack.com/EricEisaman/cs1-engine/master/dist/lib/cs1-jukebox.js",
 
-"a-jukebox": "https://raw.githack.com/EricEisaman/cs1-engine/master/dist/lib/cs1-jukebox.js"
+"a-jukebox": "https://raw.githack.com/EricEisaman/cs1-engine/master/dist/lib/cs1-jukebox.js",
+  
+"cs1-trail": "https://raw.githack.com/EricEisaman/cs1-engine/master/dist/lib/cs1-trail.js"
 
 
 }
--- src/game/main.js
+++ src/game/main.js
@@ -7,11 +7,56 @@ CS1.create('a-jukebox').then(o=>{
   o.setAttribute('scale','4 4 4')
   CS1.scene.appendChild(o);
 })
-CS1.create('a-box').then(o=>{
-  o.setAttribute('color','blue');
-  o.setAttribute('scale','5 10 5');
-  o.setAttribute('position','-4 5 -15');
-  CS1.scene.appendChild(o);
+CS1.registerComponent('circle-movement', {
+  schema: {
+    center: {type: 'vec3'},
+    speed: {default: 1},
+    radius: {default: 20},
+    reverse: {default: false}
+  },
+  
+  init: function(){
+    this.dir=this.data.reverse?1:-1;
+  },
+  
+  tick: function(t,dt){
+    // x = rCos(theta)   y = rSin(theta)
+    const x = this.data.center.x+this.data.radius*Math.cos(this.dir*t*this.data.speed/1000);
+    const y = this.data.center.y;
+    const z = this.data.center.z+this.data.radius*Math.sin(this.dir*t*this.data.speed/1000);
+    this.el.object3D.position.set(x, y, z);
+  }
+  
 });
+
+
 CS1.game.addEnvironment();
 CS1.game.start();
+
+
+
+
+
+
+
+
+document.addEventListener('gameStart',e=>{
+    
+  addCircleFlyer('blue','0 6 -20',false,1);
+  addCircleFlyer('red','-4 4 -15',true,1.5);
+  addCircleFlyer('white','4 8 -25',true,0.6);
+  
+     
+});  
+  
+function addCircleFlyer(color,center,reverse,speed){
+  const s = document.createElement('a-sphere');
+  s.setAttribute('trail__red','color:red; offset:-1 0 0');
+  s.setAttribute('trail__white','color:white; length:120');
+  s.setAttribute('trail__blue','color:blue; offset:1 0 0');
+  s.setAttribute('color',color);
+  s.setAttribute('radius', 0.5);
+  s.setAttribute('circle-movement',`center:${center}; radius: 6;reverse:${reverse};speed:${speed}`);
+  CS1.scene.appendChild(s);
+}
+
