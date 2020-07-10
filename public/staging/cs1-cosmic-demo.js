94a420887e2c59ff07c79a2d063722ebe757fe16
Glitch (cs1-demos) <none>
2020-07-10 18:08:38 +0000

    update cosmic demo

--- public/staging/cs1-cosmic-demo.js
+++ public/staging/cs1-cosmic-demo.js
@@ -5,7 +5,8 @@ const version = {version:'0.0.10'};
 console.log('CS1 Cosmic Demo Version: ', version.version);
 
 
-CS1.scene.set('environment',{preset:'osiris', ground:'flat'});
+await CS1.scene.set('environment',{preset:'osiris', ground:'flat'});
+CS1.add('a-light',{position:'12 12 -12', type: 'ambient' , intensity: 0.3});
 
 await CS1.scene.set('render-order','background foreground');
    
@@ -17,7 +18,8 @@ log.setAttribute('position','-5  3 -12');
 CS1.scene.appendChild(log);
 
 
-CS1.myPlayer.setAvatar({color:"orange",head:"oval",body:"box"});
+//CS1.myPlayer.setAvatar({color:"orange",head:"oval",body:"box"})
+CS1.myPlayer.setAvatar({type:"rigged"});
 
 CS1.myPlayer.setAttribute('jump','speed:25');
   
