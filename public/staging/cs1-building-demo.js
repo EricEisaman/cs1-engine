b946629cf2e38d111409c7c7f5e3d507c1431eb3
Glitch (cleanup-nomenclature) <none>
2020-07-12 23:22:06 +0000

    update staging and prod demos

--- public/staging/cs1-building-demo.js
+++ public/staging/cs1-building-demo.js
@@ -1,28 +1,28 @@
-const version = {version:'0.0.10'};
+const Version = {version:'0.0.10'};
 
 (async ()=>{
 
-console.log('CS1 Building Demo Version: ', version.version);
+console.log('CS1 Building Demo Version: ', Version.version);
 
 
-CS1.scene.set('environment',{preset:'default', ground:'flat'});
+CS1.Scene.set('environment',{preset:'default', ground:'flat'});
 
-await CS1.scene.set('render-order','background foreground');
+await CS1.Scene.set('render-order','background foreground');
    
 const juke = await CS1.add('a-jukebox',{position:'0 2 -1.5', scale:'2 2 2'});    
 juke.set('render-order','background');
   
 const log = document.createElement('a-log');
 log.setAttribute('position','-5  3 -12');
-CS1.scene.appendChild(log);
+CS1.Scene.appendChild(log);
 
 
-CS1.myPlayer.setAvatar({color:"#220907",
+CS1.MyPlayer.setAvatar({color:"#220907",
                        head:"https://cdn.glitch.com/aa685e59-3fcd-4b08-befb-d37b89a08db9%2Fdemon_head.glb?v=1593789346807"});
 
-CS1.myPlayer.setAttribute('jump','speed:25');
+CS1.MyPlayer.setAttribute('jump','speed:25');
   
-CS1.game.start();
+CS1.Game.start();
 
 
   
