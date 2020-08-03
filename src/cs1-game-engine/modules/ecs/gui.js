69ada8450b6fa139f66c49d3d18b2291848f270a
Glitch (cs1-demos) <none>
2020-08-03 03:33:08 +0000

    gui module created but not used

---
+++ src/cs1-game-engine/modules/ecs/gui.js
@@ -0,0 +1,51 @@
+export const gui = (()=>{
+  
+AFRAME.registerSystem('gui', {
+  schema: {},  
+
+  init: function () {
+   
+  },
+
+ 
+});
+
+
+AFRAME.registerComponent('gui', {
+
+	schema: {
+		
+	},
+  
+  init: function(){
+    
+   
+    
+  },
+  
+  update: function () {
+    
+  },
+
+	tick: function () {
+		
+	}
+  
+});
+  
+  
+  
+AFRAME.registerPrimitive('a-gui', {
+  defaultComponents: {
+    mission:{}
+  },
+
+  mappings: {
+    
+  }
+});  
+
+  
+  
+  
+})()
